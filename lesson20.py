8673808c451632bb85bdc9071886181bc7bb482e
CDCfishzz <cdcfishzz@gmail.com>
2018-11-02 02:16:46 +0800

    #Mixin

--- lesson20.py
+++ lesson20.py
@@ -6,3 +6,65 @@ class Mammal(Animal):
     pass
 
 
+class Bird(Animal):
+    pass
+
+
+class RunnableMixIn(object):
+    def run(self):
+        print('Running...')
+
+
+class FlyableMixIn(object):
+    def fly(self):
+        print('Flying...')
+
+
+class CarnivorousMixIn(Animal):
+    pass
+
+
+class HerbivoresMixIn(Animal):
+    pass
+
+
+class Dog(Mammal, RunnableMixIn, CarnivorousMixIn):
+    pass
+
+
+class Bat(Mammal, FlyableMixIn, HerbivoresMixIn):
+    pass
+
+# 2 tcp udp forking threading co-routine
+
+
+class TCPServer(object):
+    pass
+
+
+class UDPServer(object):
+    pass
+
+
+class ForkingMixIn(object):
+    pass
+
+
+class ThreadingMixIn(object):
+    pass
+
+
+class CoroutineMixIn(object):
+    pass
+
+
+class MyTCPServer(TCPServer, ForkingMixIn):
+    pass
+
+
+class MyUDPServer(UDPServer, ThreadingMixIn):
+    pass
+
+
+class MyTCPServer2(TCPServer, CoroutineMixIn):
+    pass
