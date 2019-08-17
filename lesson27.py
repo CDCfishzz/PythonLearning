75690e071306cc07845d724ddaf25b28ffcf250a
fishzz <cdcfishzz@gmail.com>
2019-08-17 14:04:05 +0800

    update lesson27

--- lesson27.py
+++ lesson27.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
-from multiprocessing import Process
+import random
+import time
+import subprocess
+from multiprocessing import Process, Pool
 
 
 def run_proc(name):
     print('Run child process %s(%s)...' % (name, os.getpid()))
+
+
+def long_time_task(name):
+    print('run task %s(%s)...' % (name, os.getppid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
 
 
 if __name__ == '__main__':
@@ -14,3 +25,17 @@ if __name__ == '__main__':
     p.start()
     p.join()
     print('Child process end.')
+
+    p2 = Pool(4)
+    for i in range(5):
+        p2.apply_async(long_time_task, args=(1,))
+    print('Waiting for all subprocesses done...')
+    p2.close()
+    p2.join()
+    print('All subprocesses done.')
+
+    print('$ nslookup www.python.org')
+    r = subprocess.call(['nslookup', 'www.python.org'])
+    print('Exit code:', r)
+
+
